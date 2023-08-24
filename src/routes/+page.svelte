c798d1bae851d57b1580c3ebabbbf7941eea3fcc
ashirbadsahu <ashirbadsahu300@gmail.com>
2023-08-24 20:10:03 +0530

    added login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,4 @@
 
 </script>
 <Editor />
+
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import supabase from "$lib/db";
+    import { user } from "$lib/index";
+    import { redirect } from '@sveltejs/kit'
+
+    let email = "";
+    let password = "";
+    const signUp = async () => {
+        let { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+        throw redirect('/')
+    };
+
+
+    const logIn = async () => {
+        let { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+        
+       throw redirect('/');
+       
+  };
+
+</script>
+<div class="mx-10 p-2 my-10">
+    <label for="email">
+    Email:
+        <input type="email" bind:value={email} placeholder=" enter email here">
+    </label>
+    <br><br>
+    <label for="password">
+        Password:
+            <input type="password" bind:value={password} placeholder=" enter password here">
+    </label>
+    <br><br>
+    <button class="bg-red-700 text-xl p-2 rounded-lg " on:click={signUp}>SignUp</button>
+    <button class="bg-red-700 text-xl p-2 rounded-lg " on:click={logIn}>LogIn</button>
+</div>
