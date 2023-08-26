78e88a56449cb7ed88cf31cdcf05eed83642f066
ashirbadsahu <ashirbadsahu300@gmail.com>
2023-08-26 20:25:53 +0530

    fixed login data reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script>
     import Editor from "../lib/Editor.svelte";
+    import { user } from "$lib/stores";
     import supabase from "$lib/db";
-    import { user } from "$lib/index";
-
+    user.set(supabase.auth.user());
+    console.log(supabase.auth.user())
+    
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session?.user)
+    })
 </script>
 <Editor />
+
+    <h4>Welcome {$user.email}</h4>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,49 +1,53 @@
 <script>
     import supabase from "$lib/db";
     import { goto } from "$app/navigation";
-    import { user } from "$lib/index";
     let email = "";
     let password = "";
     const signUp = async () => {
-        let { data:userData, error } = await supabase.auth.signUp({
+        let { data, error } = await supabase.auth.signUp({
             email: email,
             password: password,
         });
-        if (userData) {
-        user = userData;
-        goto('/')
-     }
-        // throw redirect('/')
+        goto("/");
     };
 
-
     const signIn = async () => {
-   let { data:userData, error } = await supabase.auth.signIn({
-   email: email,
-   password: password
-    })
-     
-     if (userData) {
-        user = userData;
-        goto('/')
-     }
-  };
-
+        let { data, error } = await supabase.auth.signIn({
+            email: email,
+            password: password,
+        });
+        goto("/");
+    };
 </script>
+
 <div class="mx-10 p-2 my-10">
     <label for="email">
         Email<span class="text-red-700 text-sm">* </span>
-        <input type="email" bind:value={email} placeholder=" enter email here">
+        <input
+            type="email"
+            bind:value={email}
+            placeholder=" enter email here"
+            class="mb-2 border border-gray-200"
+        />
     </label>
-    <br><br>
+    <br /><br />
     <label for="password">
         Password<span class="text-red-700 text-sm">* </span>:
-            <input type="password" bind:value={password} placeholder=" enter password here">
-    </label><br><br>
+        <input
+            type="password"
+            bind:value={password}
+            placeholder=" enter password here"
+            class="mb-2 border border-gray-200"
+        />
+    </label><br /><br />
     {#if password.length < 8}
         password must be 8 characters long
     {/if}
-    <br><br>
-    <button class="bg-red-700 text-xl p-1 rounded-lg " on:click={signUp}>SignUp</button>
-    <button class="bg-red-700 text-xl p-1 rounded-lg " on:click={signIn}>LogIn</button>
+    <br /><br />
+    <button class="bg-red-700 text-xl p-1 rounded-lg" on:click={signUp}
+        >SignUp</button
+    >
+    <button class="bg-red-700 text-xl p-1 rounded-lg" on:click={signIn}
+        >LogIn</button
+    >
 </div>
